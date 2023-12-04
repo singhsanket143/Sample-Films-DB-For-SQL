97e5bee00c332a5ae4306d226b9809d05d573ed3
Sanket Singh <singhsanket143@gmail.com>
2023-12-04 21:05:01 +0530

    Create 2 LIST DOWN TABLES IN SQL.sql

---
+++ 2 LIST DOWN TABLES IN SQL.sql
@@ -0,0 +1,10 @@
+-- To perform any operation on a DB, we need to select the DB first
+-- Because there might a lot of DBs in your machine, which DB do you target
+-- to run the query on, is identified using the `USE db_name` command
+
+
+USE LCO_FILMS;
+
+-- How to get a list of all tables in a db ?
+
+SHOW TABLES;
