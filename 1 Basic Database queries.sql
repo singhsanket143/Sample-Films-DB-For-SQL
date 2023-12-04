8ec75dc6b1e668cc16b5fcd58dc81fc6433fba8c
Sanket Singh <singhsanket143@gmail.com>
2023-12-04 21:01:35 +0530

    Create 1 Basic Database queries.sql

---
+++ 1 Basic Database queries.sql
@@ -0,0 +1,13 @@
+-- Data query (Read) , Data manipulation (Update), Data createion (Create) , Delete Data 
+-- SQL is case insensitive
+-- 1. To see all available DBS
+
+SHOW DATABASES; 
+
+-- 2. Delete a database, HERE DROP refers to delete
+
+DROP DATABASE FITNESS_DB;
+
+-- 3. Create database
+
+CREATE DATABASE Sample_Demo_DB;
